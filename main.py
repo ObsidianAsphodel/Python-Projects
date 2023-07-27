45f198964c64cba44e5e4b89ff213a82d0ac41fa
ObsidianAsphodel <terencep.dixon@gmail.com>
2023-07-27 14:05:24 -0700

    added to main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ num1 = 5
 num2 = 10
 print("Hello World")
 print(num1 + num2)
+
+def function1 (x,  y):
+    if x >= y:
+        print(x, "is bigger than", y)
+    elif y >= x:
+        print(y, "is bigger than", x)
+function1(num1, num2)
